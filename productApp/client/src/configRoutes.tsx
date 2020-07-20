130b748e2b6d3a0d56d9c0c8a79dbc06e0f40b86
durgamatha@6 <sumansukumar26@yahoo.com>
2020-07-20 23:48:34 +0530

    first commit

---
+++ productApp/client/src/configRoutes.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { Route, IndexRoute, Redirect } from 'react-router';
+
+import App from './components/App';
+
+
+
---
+++ productApp/client/src/main.tsx
@@ -0,0 +1,6 @@
+// React and Hot Loader
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+
+import { browserHistory as history } from 'react-router';
